a797f5674eda136315d9b5f67100b31649a24f03
bwortmanUNO <47804573+bwortmanUNO@users.noreply.github.com>
2019-04-24 16:59:16 -0500

    Add files via upload

---
+++ ChatClient.py
@@ -0,0 +1,50 @@
+import socket
+import select
+import sys
+import errno
+
+HEADER = 10
+HOST = 'localhost'
+PORT = 9876
+
+my_username = input("Username: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect()
+client.setblocking(False)
+
+username = my_username.encode("utf-8")
+username_header = f"{len(username):<{HEADER}}".encode("utf-8")
+client.send(username_header + username)
+
+while True:
+    message = input(f"{my_username} > ")
+
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER}}".encode("utf-8")
+        client.send(message_header+message)
+    try:
+        while True:
+            username_header = client.recv(HEADER)
+
+            if not len(username_header):
+                print("Connection closed by the server")
+                sys.exit()
+            username_length = int(username_header.decode("utf-8"))
+            username = client.recv(username_length.decode("utf-8"))
+
+            message_header = client.recv(HEADER)
+            message_length = int(message_header.decode("utf-8"))
+            message = client.recv(message_length.decode("utf-8"))
+
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN or e.errno != errno.EWOULDBLOCK:
+            print('reading error', str(e))
+            sys.exit()
+        continue
+
+    except Exception as e:
+        print('Error:', str(e))
+        sys.exit()
---
+++ ChatServer.py
@@ -0,0 +1,68 @@
+import socket
+import select
+
+HEADER = 10
+HOST = 'localhost'
+PORT = 9876
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SQL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server.bind((HOST, PORT))
+
+server.listen()
+
+socket_list = [server]
+
+client_list = {}
+
+
+def recv_msg(client_socket):
+    try:
+        header = client_socket.recv(HEADER)
+
+        if not len(header):
+            return False
+
+        msg_length = int(header.decode("utf-8"))
+
+        return {"header": header, "data": client_socket.recv(msg_length)}
+    except:
+        return False
+
+
+while True:
+    read_sockets, write, exception_sockets = select.select(socket_list, [], socket_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server:
+            client_socket, client_address = server.accept()
+
+            user = recv_msg(client_socket)
+
+            if user is False:
+                continue
+
+            socket_list.append(client_socket)
+
+            client_list[client_socket] = user
+
+            print("New connection from ", client_address[0], ":", client_address[1])
+        else:
+            message = recv_msg(notified_socket)
+
+            if message is False:
+                print("Closed connection from ", client_list[notified_socket]['data'].decode('utf-8'))
+                socket_list.remove(notified_socket)
+                del client_list[notified_socket]
+                continue
+            user = client_list[notified_socket]
+            print("Received message from ", user['data]'].decode('utf-8'))
+
+            for client_socket in client_list:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+        socket_list.remove(notified_socket)
+        del client_list[notified_socket]
